2908e219cf3c23f5467bb202dfcd83d325fd3084
Kostya <gta5kostya@mail.ru>
2023-03-05 14:42:10 +0300

    fuckU

---
+++ break.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(){
+	for (int i = 0; i < 10000; i++) {
+		std::ofstream outfile(std::to_string(i) + ".txt");
+		outfile << "lol" << std::endl;
+		outfile.close();
+	}
+	return 0;
+}
